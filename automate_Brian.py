1cd0f1d919dca32a19e89aea1d88a7d1ee1d0587
bka1605 <briankristanto.alim@gmail.com>
2026-06-09 07:35:04 +0700

    Ganti Dataset

--- automate_Brian.py
+++ automate_Brian.py
@@ -4,42 +4,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 def run_preprocessing():
-    print("Memulai otomatisasi preprocessing data Iris...")
+    print("Memulai otomatisasi preprocessing data Mobile Price Classification...")
 
-    # 1. Data Loading
-    # Pastikan file iris.csv ada di dalam folder iris_raw
-    df = pd.read_csv('iris_raw/iris.csv')
+    # load data
+    df = pd.read_csv('mobile_price_cls_raw/mobile_price_cls_train.csv')
 
-    # 2. Pembersihan Data (Menghapus Missing Values & Duplicates)
+    # cleaning data
     df = df.dropna()
     df = df.drop_duplicates()
 
-    # 3. Memisahkan Fitur (X) dan Target (y)
-    X = df.drop(columns=['target'])
-    y = df['target']
+    # pisahin x dan y
+    X = df.drop(columns=['price_range'])
+    y = df['price_range']
 
-    # 4. Splitting Data (80% Train, 20% Test)
+    # data split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # 5. Scaling Fitur menggunakan StandardScaler
+    # scaling data
     scaler = StandardScaler()
     
-    # Konversi kembali output scaler menjadi Pandas DataFrame agar bisa menggunakan .to_csv
+    # ubah hasil scaling ke DataFrame biar bisa disimpan ke CSV 
     X_train_scaled = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
     X_test_scaled = pd.DataFrame(scaler.transform(X_test), columns=X_test.columns)
 
-    y_train_df = pd.DataFrame(y_train, columns=['target'])
-    y_test_df = pd.DataFrame(y_test, columns=['target'])
+    y_train_df = pd.DataFrame(y_train, columns=['price_range'])
+    y_test_df = pd.DataFrame(y_test, columns=['price_range'])
 
-    # 6. Menyimpan Hasil Preprocessing ke Folder
-    os.makedirs('iris_preprocessing', exist_ok=True)
+    # simpan ke folder
+    os.makedirs('mobile_price_cls_preprocessing', exist_ok=True)
     
-    X_train_scaled.to_csv('iris_preprocessing/X_train.csv', index=False)
-    X_test_scaled.to_csv('iris_preprocessing/X_test.csv', index=False)
-    y_train_df.to_csv('iris_preprocessing/y_train.csv', index=False)
-    y_test_df.to_csv('iris_preprocessing/y_test.csv', index=False)
+    X_train_scaled.to_csv('mobile_price_cls_preprocessing/X_train.csv', index=False)
+    X_test_scaled.to_csv('mobile_price_cls_preprocessing/X_test.csv', index=False)
+    y_train_df.to_csv('mobile_price_cls_preprocessing/y_train.csv', index=False)
+    y_test_df.to_csv('mobile_price_cls_preprocessing/y_test.csv', index=False)
 
-    print("Otomatisasi sukses! Data siap latih telah disimpan di folder 'iris_preprocessing'.")
+    print("Otomatisasi sukses! Data siap latih telah disimpan di folder 'mobile_price_cls_preprocessing'.")
 
 if __name__ == "__main__":
     run_preprocessing()
